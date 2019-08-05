23b2532f34b0ff3d5bbf2c43fa5e8e022d113573
tron <mengxiangyue@tron.network>
2019-08-05 15:07:46 +0800

    now the whole process can be finished with embed tomcat server. Also have a jarScanner class to load class form jar file

--- src/main/java/tron/deployment/Controller/deployController.java
+++ src/main/java/tron/deployment/Controller/deployController.java
@@ -1,8 +1,12 @@
 package tron.deployment.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import tron.deployment.pojo.ConfigGenerator;
+import tron.deployment.pojo.Configuration;
+import tron.deployment.shellExecutor.BashExecutor;
 
 @Controller
 @RequestMapping(value = "/")
@@ -12,5 +16,21 @@ public class deployController {
     String index(){
 
         return "index.html";
+    }
+
+    @RequestMapping(value="/config")
+    public String config(){
+        return "configSelect.html";
+    }
+
+    @RequestMapping(value = "/config/submit")
+    public String deploy(@ModelAttribute("form") Configuration configuration, Model model) {
+        model.addAttribute("configuration", configuration);
+        ConfigGenerator configGenerator = new ConfigGenerator();
+        BashExecutor bashExecutor = new BashExecutor();
+        bashExecutor.callBuildScript2();
+        configGenerator.generateConfig(configuration);
+        bashExecutor.callDeployScript();
+        return "result";
     }
 }
